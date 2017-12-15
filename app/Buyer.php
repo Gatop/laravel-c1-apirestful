9d87fc99ed03d3bf984afde9305ce7ca69285b94
Jega <gatomzl@gmail.com>
2017-12-14 23:47:03 -0500

    Laravel - APIRest - Added support for implicit injection through scopes

--- app/Buyer.php
+++ app/Buyer.php
@@ -3,9 +3,20 @@
 namespace App;
 
 use App\Transaction;
+use App\Scopes\BuyerScope;
 
 class Buyer extends User
 {
+
+    // Metodo para poder implementar los scopes
+    // Permite la inyeccion explicita de dependencia en Buyer
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new BuyerScope);
+    }
+
     public function transactions() {
         return $this->hasMany(Transaction::class);
     }
--- app/Seller.php
+++ app/Seller.php
@@ -3,9 +3,19 @@
 namespace App;
 
 use App\Product;
+use App\Scopes\SellerScope;
 
 class Seller extends User
 {
+    // Metodo para poder implementar los scopes
+    // Permite la inyeccion explicita de dependencia en Buyer
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new SellerScope);
+    }
+
     public function products() {
         return $this->hasMany(Product::class);
     }
